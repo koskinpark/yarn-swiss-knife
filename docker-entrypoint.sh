fa5f486e1c78f3e99fe7215c090049de6b098217
Konstantin Shupenko <koskinparkin@gmail.com>
2019-05-29 14:10:21 +0600

    front - removed npm-cache

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 cd /app/project
 
-mkdir -p npm-packages-offline-cache
-cp -RT /app/npm-packages-offline-cache/ ./npm-packages-offline-cache/
-
-# first offline load basic packages.
-yarn install --ignore-optional --offline
-
 # if new were added
 yarn check || yarn install --ignore-optional --check-files
 exec "$@"
